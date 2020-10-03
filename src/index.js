a698b6ba88c6fa43c3d05bd4915b137b3348c00a
Artyom <70696147+pyzhovartyom@users.noreply.github.com>
2020-10-03 10:26:30 +0300

    feat: add min, max and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (Array.isArray(array) === false || array.length === 0) {
+        return 0;
+    } else {
+        function compareNumeric(a, b) {
+            if (a > b) return 1;
+            if (a == b) return 0;
+            if (a < b) return -1;
+        }
+        array.sort(compareNumeric);
+        return array[0];
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
-}
+    if (Array.isArray(array) === false || array.length === 0) {
+        return 0;
+        } else {
+        function compareNumeric(a, b) {
+            if (a > b) return 1;
+            if (a == b) return 0;
+            if (a < b) return -1;
+        }
+        array.sort(compareNumeric);
+        return array[array.length - 1];
+        }
+    }
 
 exports.avg = function avg (array) {
-  return 0;
-}
+    if (Array.isArray(array) === false || array.length === 0) {
+        return 0;
+      } else {
+        let result = array.reduce((sum, current) => sum + current, 0);
+        return result / array.length
+      }
+    }
